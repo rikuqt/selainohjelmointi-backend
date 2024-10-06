f28c1b46a973f462b73e3add752ffd75644a96f0
rikuqt <riku.tomann@gmail.com>
2024-10-06 14:22:43 +0300

    eka commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const cors = require('cors')
+app.use(cors())
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: "1"
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: "2"
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: "3"
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: "4"
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
+  app.post('/api/persons', (request, response) => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => Number(n.id))) 
+      : 0
+  
+    const person = request.body
+    person.id = String(maxId + 1)
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    
+  
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+  
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
